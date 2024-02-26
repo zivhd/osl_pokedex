0e647950ed98c62aa675514dfe9433c54ec4af70
zivhd <56124166+zivhd@users.noreply.github.com>
2024-02-27 01:19:49 +0800

    Add search feature

--- index.js
+++ index.js
@@ -2,16 +2,35 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 const port = 3000;
-
-// EJS setup
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
-// Static files
 app.use(express.static('public'));
 app.use(express.static('scripts'));
 
-// Routes
+const cache = {};
+app.get('/search/:term', async (req, res) => {
+    try {
+        const { term } = req.params;
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000'); // super ugly way to search
+        const allPokemons = response.data.results;
+
+        const matchingPokemons = allPokemons.filter(pokemon => pokemon.name.includes(term.toLowerCase()));
+
+        const pokemonDataPromises = matchingPokemons.map(pokemon => fetchPokemonData(pokemon.url));
+        const pokemonData = await Promise.all(pokemonDataPromises);
+
+        const pokemons_with_data = matchingPokemons.map((pokemon, index) => ({
+            name: pokemon.name,
+            data: pokemonData[index]
+        }));
+
+        res.json({ pokemons_with_data });
+    } catch (error) {
+        console.error('Error searching Pokemons by name:', error);
+        res.status(404).json({ error: 'Pokemons not found' });
+    }
+});
+
 app.get('/loadMore/:offset/:limit', async (req, res) => {
     try {
         const { offset, limit } = req.params;
--- scripts/functions.js
+++ scripts/functions.js
@@ -4,49 +4,80 @@ document.addEventListener('DOMContentLoaded', () => {
     const pokemonListContainer = document.getElementById('pokemonList');
     let offset = 10; 
 
+    const searchField = document.getElementById('searchField');
+    const searchBtn = document.getElementById('searchBtn');
+    const createPokemonCard = (pokemon) => {
+        const pokemonCard = document.createElement('div');
+        pokemonCard.className = 'bg-white rounded-lg p-4 shadow-md';
+    
+   
+        const image = document.createElement('img');
+        image.className = 'mx-auto mb-4';
+        image.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.data.id.toString().padStart(3, '0')}.png`;
+        image.alt = pokemon.name;
+        pokemonCard.appendChild(image);
+    
+      
+        const name = document.createElement('p');
+        name.className = 'text-2xl';
+        name.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        pokemonCard.appendChild(name);
+    
+ 
+        const id = document.createElement('p');
+        id.className = 'text-lg mb-2';
+        id.textContent = `#${pokemon.data.id.toString().padStart(3, '0')}`;
+        pokemonCard.appendChild(id);
+    
+
+        const typesContainer = document.createElement('div');
+        typesContainer.className = 'grid grid-cols-2 gap-4';
+    
+
+        pokemon.data.types.forEach(type => {
+            const typeElement = document.createElement('div');
+            typeElement.className = 'bg-gray-300 rounded-lg p-1 shadow-md text-center';
+            typeElement.textContent = type.type.name;
+            typesContainer.appendChild(typeElement);
+        });
+    
+        pokemonCard.appendChild(typesContainer);
+        return pokemonCard;
+    };
+    const handleSearch = async () => {
+        console.log('hahaha' + searchField.value.trim());
+        const searchTerm = searchField.value.trim();
+
+        if(searchTerm !== ''){
+            try {
+                const response = await axios.get(`/search/${searchTerm}`);
+                const searchedPokemons = response.data.pokemons_with_data;
+
+
+                pokemonListContainer.innerHTML = '';
+                loadMoreBtn.setAttribute("hidden",true)
+
+                searchedPokemons.forEach(pokemon => {
+                    const pokemonCard = createPokemonCard(pokemon);
+                    pokemonListContainer.appendChild(pokemonCard);
+                });
+
+            } catch (error) {
+                console.error('Error searching Pokemons:', error);
+            }
+    }
+    };
+
+
     loadMoreBtn.addEventListener('click', async () => {
         try {
             const response = await axios.get(`/loadMore/${offset}/10`);
             const newPokemonsWithData = response.data.pokemons_with_data;
 
             newPokemonsWithData.forEach(pokemon => {
-                const pokemonCard = document.createElement('div');
-                pokemonCard.className = 'bg-white rounded-lg p-4 shadow-md';
-            
-           
-                const image = document.createElement('img');
-                image.className = 'mx-auto mb-4';
-                image.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.data.id.toString().padStart(3, '0')}.png`;
-                image.alt = pokemon.name;
-                pokemonCard.appendChild(image);
-            
               
-                const name = document.createElement('p');
-                name.className = 'text-2xl';
-                name.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-                pokemonCard.appendChild(name);
             
-         
-                const id = document.createElement('p');
-                id.className = 'text-lg mb-2';
-                id.textContent = `#${pokemon.data.id.toString().padStart(3, '0')}`;
-                pokemonCard.appendChild(id);
-            
-
-                const typesContainer = document.createElement('div');
-                typesContainer.className = 'grid grid-cols-2 gap-4';
-            
-
-                pokemon.data.types.forEach(type => {
-                    const typeElement = document.createElement('div');
-                    typeElement.className = 'bg-gray-300 rounded-lg p-1 shadow-md text-center';
-                    typeElement.textContent = type.type.name;
-                    typesContainer.appendChild(typeElement);
-                });
-            
-                pokemonCard.appendChild(typesContainer);
-            
-
+                const pokemonCard = createPokemonCard(pokemon);
                 pokemonListContainer.appendChild(pokemonCard);
             });
             
@@ -56,4 +87,6 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Error loading more Pokemons:', error);
         }
     });
+    searchBtn.addEventListener('click', handleSearch);
 });
+
